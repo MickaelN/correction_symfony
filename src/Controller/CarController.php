39d2928eebc69d86907cefcdd3dc30a9d532118f
Mickael Noel <mickael.noel@novei.fr>
2022-02-24 16:01:47 +0100

    Ajout de l'entité cars

--- src/Controller/CarController.php
+++ src/Controller/CarController.php
@@ -2,26 +2,32 @@
 
 namespace App\Controller;
 
+use App\Entity\Cars;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CarController extends AbstractController
 {
+    private Cars $car;
+    
+    public function __construct()
+    {
+        $this->car = new Cars();
+        $this->car->setBrand('Ferrari');
+        $this->car->setModel('488 GTB');
+        $this->car->setYear(2015);
+        $this->car->setEngine('V8 bi-turbo');
+        $this->car->setColor('red');
+    }
+
     /**
      * @Route("/car", name="car")
      */
     public function index(): Response
-    {
-        $carInfo = [
-            'brand' => 'Ferrari',
-            'model' => '488 GTB',
-            'year' => '2015',
-            'engine' => 'V8 bi-turbo',
-            'color' => 'red'
-        ];
+    { 
         return $this->render('car/index.html.twig', [
-            'carInfo' => $carInfo,
+            'carInfo' => $this->car,
         ]);
     }
 }
---
+++ src/Entity/Cars.php
@@ -0,0 +1,114 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CarsRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CarsRepository::class)
+ */
+class Cars
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $brand;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $model;
+
+    /**
+     * @ORM\Column(type="integer")
+     */
+    private $year;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $engine;
+
+    /**
+     * @ORM\Column(type="string", length=255)
+     */
+    private $color;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+    public function setId(int $id): self
+    {
+        $this->id = $id;
+
+        return $this;
+    }
+    public function getBrand(): ?string
+    {
+        return $this->brand;
+    }
+
+    public function setBrand(string $brand): self
+    {
+        $this->brand = $brand;
+
+        return $this;
+    }
+
+    public function getModel(): ?string
+    {
+        return $this->model;
+    }
+
+    public function setModel(string $model): self
+    {
+        $this->model = $model;
+
+        return $this;
+    }
+
+    public function getYear(): ?int
+    {
+        return $this->year;
+    }
+
+    public function setYear(int $year): self
+    {
+        $this->year = $year;
+
+        return $this;
+    }
+
+    public function getEngine(): ?string
+    {
+        return $this->engine;
+    }
+
+    public function setEngine(string $engine): self
+    {
+        $this->engine = $engine;
+
+        return $this;
+    }
+
+    public function getColor(): ?string
+    {
+        return $this->color;
+    }
+
+    public function setColor(string $color): self
+    {
+        $this->color = $color;
+
+        return $this;
+    }
+}
